beffc41ca5189f238dea55febcfd3beb6578407c
a_skl <a_skl@LAPTOP-ALEX>
2023-08-11 16:51:23 +0400

    Add project files.

---
+++ Server.cpp
@@ -0,0 +1,15 @@
+// Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "stdafx.h"
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+
+int main(int arc, char* argv[])
+{
+    WSAData wsaData;
+    std::cout << "Hello World!\n";
+}
+
