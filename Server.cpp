c1f9bc76bf4030c4b7dc17dc6402dbbbf98ea617
Alex Sklemin <a76sklemin@gmail.com>
2023-09-01 14:25:29 +0400

    final commit

--- Server.cpp
+++ Server.cpp
@@ -6,10 +6,40 @@
 #include <winsock2.h>
 #include <iostream>
 
+#pragma warning(disable: 4996)
 
-int main(int arc, char* argv[])
-{
-    WSAData wsaData;
-    std::cout << "Hello World!\n";
+int main(int argc, char* argv[]) {
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+	if (newConnection == 0) {
+		std::cout << "Error #2\n";
+	}
+	else {
+		std::cout << "Client Connected!\n";
+		char msg[256] = "Hello. It`s my first network program!";
+		send(newConnection, msg, sizeof(msg), NULL);
+	}
+
+
+	system("pause");
+	return 0;
 }
-
